e7a7fb7f42db6ac03d4e7bed12ffce99f61a763a
chris-cozy <csande9@clemson.edu>
2022-09-26 22:10:07 -0400

    Starts database connection on page load

--- Mock Program/server-side/app.js
+++ Mock Program/server-side/app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+// Database service
+const dbService = require('./dbService');
 
 //-----MODULE SETUP-----//
 const app = express();
