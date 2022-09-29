9430eaa6d68054ddc4343729e900707607772f2b
chris-cozy <csande9@clemson.edu>
2022-09-29 11:31:04 -0400

    Set up function to get all table data

--- Mock Program/server-side/dbService.js
+++ Mock Program/server-side/dbService.js
@@ -28,11 +28,34 @@ connection.connect((err) => {
 // This class will be used to hold the functions for manipulating the data
 class dbService {
     /*
-        This function grabs the instance of the class. Without it, multiple instances would be made.
+        Grabs the instance of the class. Without it, multiple instances would be made.
         The return statment checks if instance is not null. If it is, creates a new instance.
     */
     static getInstance() {
         return instance ? instance : new deService();
+    }
+
+    /*
+        Grabs all of the data
+        If there is an error, logs it to the console
+    */
+    async getAllData() {
+        try {
+            // Create a promise to handle the query. Using a resolve, reject. if query successful, it will resolve. If not, it will reject and transfer to the catch.
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM mock_table";
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                });
+            });
+
+            console.log(response);
+
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 }
 
