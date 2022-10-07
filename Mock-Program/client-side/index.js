cde1d75c01bfb576326ab8ee2bd6be02fe7eaa10
chris-cozy <csande9@clemson.edu>
2022-10-07 11:21:56 -0400

    Added structure for POST

--- Mock-Program/client-side/index.js
+++ Mock-Program/client-side/index.js
@@ -10,6 +10,40 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => loadHTMLTable(data['data']));
 });
 
+//-----POST-----//
+/*
+    Function that waits for the submit button to be pressed, then takes the argument in the input box and sends it to the back-end
+
+    There is no .catch or error handling, since we have that in the back-end.
+*/
+const addBtn = document.querySelector('#add-btn');
+addBtn.onclick = function () {
+    const nameInput = document.querySelector('#test-input');
+    // Every time we grab value and send to back-end, reset value to empty string
+    const name = nameInput.value;
+    nameInput.value = "";
+
+    // Accesses the API and transmits the data to the back-end. After a response is recieved, fulfills the .then promises
+    fetch('http://localHost:5000/insert', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ name: name })
+    })
+        .then(response => response.json)
+        .then(data => insertRowIntoTable(data['data']));
+}
+
+/*
+    This function will take data and insert it into the table as a row.
+    Takes in an array of data.
+*/
+function insertRowIntoTable(data) {
+
+}
+
+//-----TABLE LOADER-----//
 /* 
     Function that takes in data and loads it into the table
     Args: Array of data
