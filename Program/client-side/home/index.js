884a371de8582e9e5efb4060ff4648c6217984c1
Jordan Towery <jtowery@clemson.edu>
2022-10-14 14:14:58 -0400

    made very basic homepage w new links

--- Program/client-side/home/index.js
+++ Program/client-side/home/index.js
@@ -2,3 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Setting the endpoint
     fetch('http://localhost:5000/getAllAccounts');
 });
+
+var navLinks = document.getElementById("navLinks");
+    function showMenu(){
+        navLinks.style.right = "0";
+    }
+    function hideMenu(){
+        navLinks.style.right = "-400px";
+    }
