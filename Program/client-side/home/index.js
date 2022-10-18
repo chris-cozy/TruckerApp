be76dc46583632d5438db71ac72c555f899fd105
Jordan Towery <jtowery@clemson.edu>
2022-10-17 20:16:10 -0400

    added buttons to use in the future, every page has the design applied now, minor bug fixes with mobile navigation bar, cleaned up button design

--- Program/client-side/home/index.js
+++ Program/client-side/home/index.js
@@ -3,10 +3,3 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('http://localhost:5000/getAllAccounts');
 });
 
-var navLinks = document.getElementById("navLinks");
-    function showMenu(){
-        navLinks.style.right = "0";
-    }
-    function hideMenu(){
-        navLinks.style.right = "-400px";
-    }
