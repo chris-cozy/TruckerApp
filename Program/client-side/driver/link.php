b02e93b81540278859bfe1b5e0de32e0b785754c
Kyle Kissane <kkissan@clemson.edu>
2022-10-31 20:19:47 +0000

    Added link.php

---
+++ Program/client-side/driver/link.php
@@ -0,0 +1,39 @@
+<?php 
+$points = $_POST['points'];
+
+if(!empty($points)){
+    # setup db connection variables
+    $host = "localhost";
+    $databaseUser = "admin";
+    $databasePassword = "QNAXni7AkACXrEWencp9";
+    $databaseName = "Driver Account";
+
+    # creating connection
+    $conn = new mysqli($host, $databaseUser, $databasePassword, $databaseName);
+
+    # checking for error
+    if(mysqli_connect_error){
+        die('Connection Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    }
+    # choose points from the driver account database
+    else {
+        $SELECT = "SELECT points From Driver Account Where points = ?";
+        $INSERT = "INSERT Into Driver Account (points) value(?)";
+
+        # preparing
+        $statmnt = $conn->prepare($SELECT);
+        $statmnt->bind_param("i", $points);
+        $statmnt->execute();
+        $statmnt->bind_result($points);
+        $statmnt->store_result();
+
+        #unfinished
+    }
+}
+else{
+    $points = 0;
+    die();
+}
+
+
+?>
