a382044b15f67e8dcff80adbbfabf9f936a1830c
Kyle Kissane <kkissan@clemson.edu>
2022-11-29 19:45:49 +0000

    Updated points.js

--- Program/client-side/driver/points.js
+++ Program/client-side/driver/points.js
@@ -2,22 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-// Shows all point management actions by a given sponsor, for drivers to view their points history
+/* Shows all point management actions by a given sponsor, for drivers to view their points history
+    To be used for reference in js code
     const displayPointDistribution(sponsorID) {
         if (sponsorID == NULL) {
             alert("Invalid Sponsor.");
         }
         try {
             const response = await new Promise((resolve, reject) => {
-                const query = "SELECT * From Points_Management Where sponsorID = ?";
-                /*
-                 May need to only display the driver being managed and the point distribution to that driver:
                     const query1 = "SELECT Driver_Username From Points_Management Where sponsorID = ?"; 
                     const query2 = "SELECT points From Points_Management Where sponsorID = ?";
 
                     connection.query(query1, [sponsorID], (err, result) =>{...});
                     connection.query(query2, [sponsorID], (err, result) =>{...});
-                */
+                
                 connection.query(query, [sponsorID], (err, result) => {
                     if (err) reject(new Error(err.message));
                 });
@@ -29,3 +27,4 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(error);
         }
     }
+    */
