b1da1da68f55f099c9283227370e24717a6a273f
chris-cozy <csande9@clemson.edu>
2022-11-30 18:30:03 -0500

    Fixed API call error

--- Program/client-side/driver/index.js
+++ Program/client-side/driver/index.js
@@ -1,5 +1,9 @@
 let currentUser;
+let response;
+let userInfo;
+
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('LOGGED IN');
     const cognitoAuth = new URLSearchParams(window.location.search);
     const authCode = cognitoAuth.get('code');
 
@@ -10,26 +14,26 @@ document.addEventListener('DOMContentLoaded', function () {
         redirect_uri: 'https://team21.cpsc4911.com/driver/'
     }
 
-        (async () => {
-            const response = await fetch('https://team21-good-driver-program.auth.us-east-1.amazoncognito.com/oauth2/token', {
-                Method: 'POST',
-                Headers: {
-                    'Accept': 'application.json',
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': 'Basic NWc0cGI3YTNpczRzMWhjYmx0bHNmb2p1Zjk6Z2I2anRzaXUxZ2VoZGI5czBpcmNmYjZxdXNrNWZoc2IzbGdsM2VwYjZrOXFibWM3c2lv'
-                },
-                Body: body
-            })
-        });
+    async () => {
+        response = await fetch('https://team21-good-driver-program.auth.us-east-1.amazoncognito.com/oauth2/token', {
+            Method: 'POST',
+            Headers: {
+                'Accept': 'application.json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic NWc0cGI3YTNpczRzMWhjYmx0bHNmb2p1Zjk6Z2I2anRzaXUxZ2VoZGI5czBpcmNmYjZxdXNrNWZoc2IzbGdsM2VwYjZrOXFibWM3c2lv'
+            },
+            Body: body
+        })
+    };
 
-    (async () => {
-        const userInfo = await fetch('https://team21-good-driver-program.auth.us-east-1.amazoncognito.com/oauth2/userinfo', {
+    async () => {
+        userInfo = await fetch('https://team21-good-driver-program.auth.us-east-1.amazoncognito.com/oauth2/userinfo', {
             Method: 'GET',
             Headers: {
                 'Authorization': 'Bearer ' + response.access_token
             }
         })
-    });
+    };
 
     let currentUser = {
         userName: userInfo.username,
