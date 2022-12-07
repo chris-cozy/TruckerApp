592e7e3f3cc2d344e7b636205f42db071a1faaaf
chris-cozy <csande9@clemson.edu>
2022-12-07 18:13:46 -0500

    syntax update

--- Program/client-side/driver/edit.js
+++ Program/client-side/driver/edit.js
@@ -4,20 +4,20 @@ const localHost = 'http://localhost:5000/';
 const corsHeader = 'https://cors-anywhere.herokuapp.com/'
 let currentUser = null;
 
-const firstNameInput = document.querySelector('#fname');
-const lastNameInput = document.querySelector('#lname');
-const emailInput = document.querySelector('#email');
-const phoneNumInput = document.querySelector('#phone');
+let firstNameInput = document.querySelector('#fname');
+let lastNameInput = document.querySelector('#lname');
+let emailInput = document.querySelector('#email');
+let phoneNumInput = document.querySelector('#phone');
 
-const drivingExpInput = document.querySelector('#exp');
+let drivingExpInput = document.querySelector('#exp');
 
-const shippingStreetInput = document.querySelector('#street');
+let shippingStreetInput = document.querySelector('#street');
 
-const shippingCityInput = document.querySelector('#city');
+let shippingCityInput = document.querySelector('#city');
 
-const shippingStateInput = document.querySelector('#state');
+let shippingStateInput = document.querySelector('#state');
 
-const shippingZipInput = document.querySelector('#zip');
+let shippingZipInput = document.querySelector('#zip');
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -70,7 +70,6 @@ updateBtn.onclick = function () {
     }
 
     console.log(profileInfo);
-    const currentUser = user.get_instance();
 
     fetch(corsHeader + publicDNS + '/updateProfileInfo', {
         headers: {
