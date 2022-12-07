0555f4f34c854114b19b019995e77c44646626d8
chris-cozy <csande9@clemson.edu>
2022-12-06 20:59:46 -0500

    connectivity

--- Program/client-side/driver/index.js
+++ Program/client-side/driver/index.js
@@ -3,7 +3,9 @@ let tokens;
 let userInfo;
 let instance = null;
 
-const publicDNS = 'https://cors-anywhere.herokuapp.com/http://54.87.82.227:3306/';
+const corsHeader = 'https://cors-anywhere.herokuapp.com/'
+
+const publicDNS = 'http://54.87.82.227:3306/';
 const localHost = 'http://localhost:5000/';
 
 document.addEventListener('DOMContentLoaded', function () {
